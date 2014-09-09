c6937180e602dcacc828032d3fb720f8ef0c5dee
Ryan Bales <ryankbales@gmail.com>
2014-09-09 14:41:23 -0700

    precourse

---
+++ the_basics/four.rb
@@ -0,0 +1,10 @@
+dates = [1984, 1985, 1990, 1984]
+
+dates.each { |date| puts date }
+
+#or
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
--- the_basics/three.rb
+++ the_basics/three.rb
@@ -1,3 +1,10 @@
 movies = { "Ghostbusters" => 1984, "Goonies" => 1985, "Dances with Wolves" => 1990, "The Never Ending Story" => 1984  }
 
 movies.each { |k, v| puts v }
+
+#or
+
+puts movies["Ghostbusters"]
+puts movies["Goonies"]
+puts movies["Dances with Wolves"]
+puts movies["The Never Ending Story"]
