2c08f147a94b21d82edfe147870d12e5ce7a1df6
Ryan Bales <ryankbales@gmail.com>
2014-09-08 22:12:56 -0700

    the basics exercise

---
+++ the_basics/one.rb
@@ -0,0 +1,1 @@
+puts "Ryan" + " Bales"
---
+++ the_basics/two.rb
@@ -0,0 +1,8 @@
+thousands = 6789 / 1000
+hundreds = 6789 % 1000 / 100
+tens = 6789 % 1000 % 100 / 10
+ones = 6789 % 1000 %100 % 10
+puts thousands
+puts hundreds
+puts tens
+puts ones
