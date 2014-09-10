f858ff1153c8749e667f220514f2613093d32f42
Ryan Bales <ryankbales@gmail.com>
2014-09-09 21:00:06 -0700

    precourse work

--- 03_variables/name.rb
+++ 03_variables/name.rb
@@ -1,5 +1,8 @@
-puts "What is your name?"
-name = gets.chomp
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+name = first_name + " " + last_name
 puts "Hello #{name}!"
 
 10.times do
---
+++ 03_variables/six.rb
@@ -0,0 +1,1 @@
+#The variable or method shoes does not exist or is outside of the scope.
